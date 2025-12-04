235454f84830bd317d1dbc6467e51f42ffb6dfe4
stinelock <eaa24stlo@students.eaaa.dk>
2025-12-04 15:08:49 +0100

    feat: add NavBar and Footer components, update layout structure

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="fixed bottom-0 bg-dark-grey w-full p-3">
+      <p>© 2025 g-bar. All rights reserved.</p>
+    </footer>
+  );
+}
