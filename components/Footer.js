b92e008ca62f38c8264d80d148b567a642d2b82a
stinelock <eaa24stlo@students.eaaa.dk>
2025-12-05 12:47:34 +0100

    feat: refactor Footer component to use direct image paths and improve layout structure

--- components/Footer.js
+++ components/Footer.js
@@ -1,16 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
 
-import logo from "@/img/logo.png";
-import FaceBook from "@/img/fb.png";
-import Instagram from "@/img/instagram.png";
-
 export default function Footer() {
   return (
-    <footer className="grid sm:grid-rows-2 gap-5 fixed bottom-0 bg-dark-grey w-full h-80 py-8 px-20 md:grid md:grid-cols-2 md:grid-rows-none md:h-55 md:px-40">
+    <footer className="grid sm:grid-rows-2 gap-5 fixed bottom-0 bg-dark-grey w-screen h-80 py-8 px-20 md:grid md:grid-cols-2 md:grid-rows-none md:h-55 md:px-40">
       <div className="flex flex-col justify-between md:justify-start md:gap-2">
         <Link href="/">
-          <Image src={logo} alt="G-bar" width="150" height="100"></Image>
+          <Image src="/img/logo.png" alt="G-bar" width="150" height="100"></Image>
         </Link>
         <Link href="https://www.google.com/maps/place/GBAR/@56.155149,10.2088994,17z/data=!3m1!4b1!4m6!3m5!1s0x464c3f90e813d1cb:0x6a32849dfa42970d!8m2!3d56.155146!4d10.2114743!16s%2Fg%2F1tdkw8jv?authuser=0&entry=ttu&g_ep=EgoyMDI1MTIwMi4wIKXMDSoASAFQAw%3D%3D">
           <address>Skolegade 28, 8000 Aarhus</address>
@@ -27,7 +23,7 @@ export default function Footer() {
         <div className="flex flex-row">
           <Link href="https://www.instagram.com/gbaraarhus/?hl=da">
             <Image
-              src={Instagram}
+              src="/img/instagram.png"
               alt="G-bar Instagram"
               width="50"
               height="100"
@@ -36,7 +32,7 @@ export default function Footer() {
           </Link>
           <Link href="https://www.facebook.com/Ilovegbar">
             <Image
-              src={FaceBook}
+              src="/img/fb.png"
               alt="G-bar Facebook"
               width="50"
               height="100"
--- components/NavBar.js
+++ components/NavBar.js
@@ -1,7 +1,27 @@
+import Link from "next/link";
+import Image from "next/image";
+
 export default function NavBar() {
   return (
-    <nav>
-      <h1>NavBar Component</h1>
+    <nav className="bg-dark-grey grid grid-cols-3 p-5 w-screen">
+      <Link href="/" className="col-start-2">
+        <Image src="/img/logo.png" alt="G-bar" width="180" height="100"></Image>
+      </Link>
+      <Image
+        src="/img/burgermenu.png"
+        alt="menu"
+        width="30"
+        height="20"
+        className="justify-self-end"
+      ></Image>
+      {/* <ul>
+        <li>
+          <Link href="/booking">Book Bord</Link>
+        </li>
+        <li>
+          <Link href="/drinks">Bestil Drinks</Link>
+        </li>
+      </ul> */}
     </nav>
   );
 }
