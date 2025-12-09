22b1343e340036c6fb497da108ab00263214cd93
stinelock <eaa24stlo@students.eaaa.dk>
2025-12-09 09:39:02 +0100

    feat: add dynamic booking types with images and descriptions to booking page

--- app/booking/page.js
+++ app/booking/page.js
@@ -1,10 +1,30 @@
 import TypeCard from "@/components/TypeCard";
 
+
 export default function BookingType() {
+
+const types = [
+  {
+    id: 1,
+    name: "Standard Bord",
+    description: "Rygning ikke tilladt",
+    image: "/img/standardbord.png",
+    icon: "/img/rygning-nej.png",
+  },
+  {
+    id: 2,
+    name: "Opvarmet Tagterrasse",
+    description: "Rygning tilladt",
+    image: "/img/tagterrasse.jpg",
+    icon: "/img/rygning-ja.png",
+  },
+];
+
   return (
     <section>
-      <TypeCard />
-      <TypeCard />
+      {types.map((type) => (
+        <TypeCard key={type.id} type={type} />
+      ))}
     </section>
   );
 }
--- components/TypeCard.js
+++ components/TypeCard.js
@@ -1,15 +1,29 @@
 import Link from "next/link";
 import Image from "next/image";
 
-export default function TypeCard() {
+export default function TypeCard({ type }) {
   return (
     <Link href="/booking/guests">
-      <div className="bg-white w-full h-auto mb-10 p-6">
-        <div className="w-auto h-auto mb-4 overflow-hidden border border-amber-600 ">
-          <Image src="/img/standardbord.png" alt="standard bord" width="200" height="100" className="w-full h-auto object-fill object-center"></Image>
+      <div className="bg-white w-full h-auto mb-10 p-6 grid grid-cols-1 gap-4">
+        <div className="w-auto h-auto mb-4 overflow-hidden">
+          <Image
+            src={type.image}
+            alt="standard bord"
+            width="200"
+            height="100"
+            className="w-full h-auto object-fill object-center"
+          ></Image>
         </div>
-        <h2 className="text-black">Standard Bord</h2>
-        <p className="text-black"> Rygning tilladt</p>
+        <h2 className="text-black">{type.name}</h2>
+        <div className="flex flex-row">
+          <Image
+            src={type.icon}
+            alt="rygning tilladt ikon"
+            width="30"
+            height="30"
+          ></Image>
+          <p className="text-black ml-4 self-center"> Rygning tilladt</p>
+        </div>
       </div>
     </Link>
   );
