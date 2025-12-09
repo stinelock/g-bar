b0e217e93fd9d6c213d8c2799f09a3cc2a97f6d9
stinelock <eaa24stlo@students.eaaa.dk>
2025-12-09 10:13:08 +0100

    feat: implement booking type selection with routing to guests page

--- app/booking/page.js
+++ app/booking/page.js
@@ -1,29 +1,42 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import TypeCard from "@/components/TypeCard";
 
-
 export default function BookingType() {
+  const router = useRouter();
 
-const types = [
-  {
-    id: 1,
-    name: "Standard Bord",
-    description: "Rygning ikke tilladt",
-    image: "/img/standardbord.png",
-    icon: "/img/rygning-nej.png",
-  },
-  {
-    id: 2,
-    name: "Opvarmet Tagterrasse",
-    description: "Rygning tilladt",
-    image: "/img/tagterrasse.jpg",
-    icon: "/img/rygning-ja.png",
-  },
-];
+  const types = [
+    {
+      id: 1,
+      name: "Standard Bord",
+      description: "Rygning ikke tilladt",
+      image: "/img/standardbord.png",
+      icon: "/img/rygning-nej.png",
+    },
+    {
+      id: 2,
+      name: "Opvarmet Tagterrasse",
+      description: "Rygning tilladt",
+      image: "/img/tagterrasse.jpg",
+      icon: "/img/rygning-ja.png",
+    },
+  ];
+
+  const handleTypeSelect = (type) => {
+    localStorage.setItem("booking", JSON.stringify({ type }));
+
+    router.push("/booking/guests");
+  };
 
   return (
     <section className="md:flex md:flex-row md:justify-center gap-8">
       {types.map((type) => (
-        <TypeCard key={type.id} type={type} />
+        <TypeCard
+          key={type.id}
+          type={type}
+          onClick={() => handleTypeSelect(type)}
+        />
       ))}
     </section>
   );
--- components/TypeCard.js
+++ components/TypeCard.js
@@ -1,30 +1,27 @@
-import Link from "next/link";
 import Image from "next/image";
 
 export default function TypeCard({ type }) {
   return (
-    <Link href="/booking/guests">
-      <div className="bg-white w-full h-auto mb-10 p-6 grid grid-cols-1 gap-4 md:max-w-2xl">
-        <div className="w-auto h-auto mb-4 overflow-hidden">
-          <Image
-            src={type.image}
-            alt="standard bord"
-            width="200"
-            height="100"
-            className="w-full h-auto object-fill object-center"
-          ></Image>
-        </div>
-        <h2 className="text-black text-5xl md:text-3xl">{type.name}</h2>
-        <div className="flex flex-row">
-          <Image
-            src={type.icon}
-            alt="rygning tilladt ikon"
-            width="30"
-            height="30"
-          ></Image>
-          <p className="text-black ml-4 self-center"> Rygning tilladt</p>
-        </div>
+    <div className="bg-white w-full h-auto mb-10 p-6 grid grid-cols-1 gap-4 md:max-w-2xl">
+      <div className="w-auto h-auto mb-4 overflow-hidden">
+        <Image
+          src={type.image}
+          alt="standard bord"
+          width="200"
+          height="100"
+          className="w-full h-auto object-fill object-center"
+        ></Image>
       </div>
-    </Link>
+      <h2 className="text-black text-5xl md:text-3xl">{type.name}</h2>
+      <div className="flex flex-row">
+        <Image
+          src={type.icon}
+          alt="rygning tilladt ikon"
+          width="30"
+          height="30"
+        ></Image>
+        <p className="text-black ml-4 self-center"> Rygning tilladt</p>
+      </div>
+    </div>
   );
 }
