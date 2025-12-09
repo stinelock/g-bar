74f9be431d1dd0b55fb6cccf377f286d7117d00d
stinelock <eaa24stlo@students.eaaa.dk>
2025-12-09 15:03:41 +0100

    feat: create BookingDetails component to display dynamic booking information

--- app/booking/info/page.js
+++ app/booking/info/page.js
@@ -1,35 +1,14 @@
-'use client'; //Bruger localStorage
+"use client";
 
 import CTAButton from "@/components/CTAButton";
+import BookingDetails from "@/components/BookingDetails";
 
-export default function BookingInfo() {
-
-  const currentBooking = JSON.parse(localStorage.getItem("booking")) || {};
+export default function BookingInfoPage() {
+ 
 
   return (
     <>
-      <section className="grid grid-cols-1 gap-4 md:grid-cols-2">
-        <h2 className="md:col-span-2">Booking Oplysninger</h2>
-        <div>
-          <h3>Adresse</h3>
-          <p>G-bar</p>
-          <p>Skolegade 28, 8000 Aarhus</p>
-        </div>
-        <div>
-          <h3>Område</h3>
-          <p>{currentBooking.type?.name}</p>
-        </div>
-        <div>
-          <h3>Antal gæster</h3>
-          <p>{currentBooking.guests} personer</p>
-        </div>
-        <div>
-          <h3>Dato & tid</h3>
-          <p>
-            {currentBooking.date} kl. {currentBooking.time}
-          </p>
-        </div>
-      </section>
+      <BookingDetails />
       <section className="mt-8">
         <h2>Personlige oplysninger</h2>
         <p>Log ind og optjen point til rabatter!</p>
