8effc3a34cc88032a3824d2f8058b12302af1deb
stinelock <eaa24stlo@students.eaaa.dk>
2025-12-09 10:44:32 +0100

    feat: update BookingSquare to use value prop for rendering guest numbers

--- components/BookingSquare.js
+++ components/BookingSquare.js
@@ -1,7 +1,7 @@
-export default function BookingSquare({key}) {
+export default function BookingSquare({value}) {
     return(
         <div className="flex self-center justify-center items-center aspect-square w-full p-3 bg-white cursor-pointer">
-            <h2 className="text-4xl font-black text-black">{key}</h2>
+            <h2 className="text-4xl font-black text-black">{value}</h2>
         </div>
     )
 }
--- components/BookingSquareGrid.js
+++ components/BookingSquareGrid.js
@@ -2,15 +2,26 @@ import BookingSquare from "./BookingSquare";
 
 export default function BookingSquareGrid({ page }) {
   if (page === "guests") {
-    const values = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const values = [
+      { key: 2, value: 2 },
+      { key: 3, value: 3 },
+      { key: 4, value: 4 },
+      { key: 5, value: 5 },
+      { key: 6, value: 6 },
+      { key: 7, value: 7 },
+      { key: 8, value: 8 },
+      { key: 9, value: 9 },
+      { key: 10, value: 10 },
+    ];
     console.log("Rendering BookingSquareGrid for guests");
 
     return (
       <div className="grid grid-cols-3 gap-8 w-full max-w-lg self-center">
-        {values.map((number) => BookingSquare({ key: number }))}
+        {values.map((guests) => (
+          <BookingSquare key={guests.key} value={guests.value} />
+        ))}
       </div>
     );
-
   } else if (page === "time") {
     const values = [2, 3, 4, 5, 6, 7, 8, 9, 10];
 
