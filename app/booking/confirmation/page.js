cd80a3d485e70eac2ab8a2f36e43fe1240c388b6
stinelock <eaa24stlo@students.eaaa.dk>
2025-12-11 12:34:52 +0100

    feat: update layout and loading behavior for images on booking confirmation page

--- app/booking/confirmation/page.js
+++ app/booking/confirmation/page.js
@@ -3,27 +3,28 @@ import Image from "next/image";
 export default function BookingConfirmationPage() {
   return (
     <section>
-      <div>
+      <div className="flex flex-row justify-center">
         <Image
           src="/img/silhouette1-w.png"
           alt="dansende silhouette"
           width="50"
           height="50"
-          className="w-24 h-auto mx-auto mt-8 opacity-30 "
+          loading="eager"
+          className="w-24 h-auto mt-8 opacity-30 "
         />
         <Image
           src="/img/silhouette2-w.png"
           alt="dansende silhouette"
           width="50"
           height="50"
-          className="w-24 h-auto mx-auto mt-8 opacity-30 "
+          className="w-24 h-auto mt-8 opacity-30 "
         />
         <Image
           src="/img/silhouette3-w.png"
           alt="dansende silhouette"
           width="50"
           height="50"
-          className="w-24 h-auto mx-auto mt-8 opacity-30 "
+          className="w-24 h-auto mt-8 opacity-30 "
         />
       </div>
       <div className="w-full h-auto p-8 bg-white text-black">
