1c8296f27d131157ffdd6ae09981df16052cdaa8
stinelock <eaa24stlo@students.eaaa.dk>
2025-12-12 15:36:40 +0100

    feat: add isConfirmationPage prop to PreviousButton for dynamic styling

--- components/PreviousButton.js
+++ components/PreviousButton.js
@@ -3,34 +3,36 @@
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 
-export default function PreviousButton({ text, href }) {
+export default function PreviousButton({ text, href, isConfirmationPage }) {
   const currentPath = usePathname();
 
-  if (!href){
-switch (currentPath) {
-  case "/booking":
-    return null;
-  case "/booking/guests":
-    href = "/booking";
-    break;
-  case "/booking/date":
-    href = "/booking/guests";
-    break;
-  case "/booking/time":
-    href = "/booking/date";
-    break;
-  case "/booking/info":
-    href = "/booking/time";
-    break;
-  default:
-    return null;
-}
+  if (!href) {
+    switch (currentPath) {
+      case "/booking":
+        return null;
+      case "/booking/guests":
+        href = "/booking";
+        break;
+      case "/booking/date":
+        href = "/booking/guests";
+        break;
+      case "/booking/time":
+        href = "/booking/date";
+        break;
+      case "/booking/info":
+        href = "/booking/time";
+        break;
+      default:
+        return null;
+    }
   }
 
   return (
     <Link
       href={href}
-      className="flex justify-center p-2 mt-4 w-full border md:mt-10 border-white"
+      className={`flex justify-center p-2 mt-4 border md:mt-10 border-white ${
+        isConfirmationPage ? "w-full" : "w-30"
+      }`}
     >
       {text}
     </Link>
