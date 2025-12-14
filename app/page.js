e85727f0a52d5fcd5c780d1337fdf24c4fa2c0d5
stinelock <eaa24stlo@students.eaaa.dk>
2025-12-14 13:03:30 +0100

    feat: enhance layout and responsiveness of home page and background image

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <main className="relative h-[1150px]">
+    <main className="relative h-[1150px] md:max-w-4xl md:w-3xl md:justify-self-center md:pb-12">
       <BackgroundImage />
       <div className="flex flex-col items-center pt-50">
         <h1 className="font-molend text-lg z-10">Velkommen til</h1>
@@ -16,16 +16,16 @@ export default function HomePage() {
           className="w-70 z-10"
         />
       </div>
-      <section className="flex flex-col mx-10 my-20 h-auto gap-10 py-20">
+      <section className="flex flex-col mx-10 my-20 h-auto gap-10 py-20 md:flex-row md:justify-center">
         <Link
           href="/booking"
-          className="bg-white z-10 text-black flex px-4 py-3 font-molend justify-center"
+          className="bg-white z-10 text-black flex px-4 py-3 font-molend justify-center md:w-1/3"
         >
           Book Bord
         </Link>
         <Link
           href="/drinks"
-          className="bg-white z-10 text-black flex px-4 py-3 font-molend justify-center"
+          className="bg-white z-10 text-black flex px-4 py-3 font-molend justify-center md:w-1/3"
         >
           bestil drinks
         </Link>
